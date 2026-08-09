bb2ed2d32d52d12b4da4178672e333c298f9a4ca
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-08-09 22:16:12 +0530

    main windows structure added

---
+++ text_editor.py
@@ -0,0 +1,11 @@
+# creating a text editor using tkinter
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+# main windows structure
+root = tk.Tk()
+root.title("Text Editor")
+root.geometry("800x600")
+
+# initiates and keep windows open until user closes it
+root.mainloop()
