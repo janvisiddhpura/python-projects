cd1ebdaba513e1ca1bf778d4b70260cd1fafde49
Janvi Siddhpura <janvisiddhpura111@gmail.com>
2026-08-10 17:53:00 +0530

    current_time added

--- digital_clock.py
+++ digital_clock.py
@@ -5,12 +5,20 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Digital Clock")
 
+def current_time():
+    # current time in hours, minutes and seconds and displays on the clock
+    time_string = time.strftime("%A, %b %d \n\n %I:%M:%S %p")
+    clock_label.config(text=time_string)
+    # update the time in every 1000 ms
+    clock_label.after(1000, current_time)
+
 clock_label = tk.Label(
     root, 
     text="Digital Clock", 
     font=("Arial", 24),
-    background="blue",
-    foreground="white"
+    background="black",
+    foreground="pale green"
 )
 clock_label.pack(anchor="center")
+current_time()
 root.mainloop()
